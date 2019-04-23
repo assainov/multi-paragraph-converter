9e4d800f4ecd6fc1b5d3712182e462d8778e8b5b
Ilyas Assainov <ilyas@prototype.net>
2019-04-23 16:41:28 +0400

    Enhanced UX

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const multiLineInput = document.querySelector('#multiline');
 const singleLineInput = document.querySelector('#singleline');
+const copied = document.querySelector('.copied');
 
 multiLineInput.addEventListener('input', function(e){
     let text = e.srcElement.value;
     text = text.replace(/(?:\r\n|\r|\n)/g, '\\n');
     singleLineInput.value = text;
+});
+
+singleLineInput.addEventListener('click', function(e){
+    if (e.srcElement.value.length > 0) {
+        singleLineInput.select();
+        document.execCommand('copy');
+        copied.classList.add('shown');
+        setTimeout(function(){
+            copied.classList.remove('shown');
+        }, 1000);
+    }
 });
